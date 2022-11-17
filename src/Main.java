0fd6c4c0aa0fd875f2a8457beef2382318ceed66
Diego <diegoandre.chavezt@gmail.com>
2022-11-17 17:45:46 -0500

    EjercicioCondicionales1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        var numeroIF = -76;
+        if (numeroIF >= 0){
+            System.out.println("Es positivo");
+        }else if (numeroIF == 0){
+            System.out.println("Tambien es positivo");
+        }else {
+            System.out.println("Es negativo");
+        }
+    }
+    }
